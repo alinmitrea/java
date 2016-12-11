f1828ff39b43bb2798724582f585241a7b25c9f3
alinmitrea <alinmitrea@gmail.com>
2016-12-11 18:07:38 +0100

    Doggy

---
+++ src/headspace/Dog.java
@@ -0,0 +1,14 @@
+package headspace;
+
+/**
+ * Created by micro on 11/12/2016.
+ */
+public class Dog {
+    int size;
+    String bread;
+    String name;
+
+    void bark(){
+        System.out.println("Ruff Ruff!");
+    }
+}
---
+++ src/headspace/DogTestDrive.java
@@ -0,0 +1,12 @@
+package headspace;
+
+/**
+ * Created by micro on 11/12/2016.
+ */
+public class DogTestDrive {
+    public static void main(String[] args){
+        Dog d = new Dog();
+        d.size = 40;
+        d.bark();
+    }
+}
